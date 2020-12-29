837d6bed3f099d86945dba437c3ec923c8e6d56d
pumposh-cyber <76180934+pumposh-cyber@users.noreply.github.com>
2020-12-28 22:19:12 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 touch index.html app.js
-npm init
+npm init -y
 npm i express body-parser
 //Steps to install nodemon https://www.npmjs.com/package/nodemon
 nodemon app.js
+//for DB we will use Mongo DB
+npm install mongodb
